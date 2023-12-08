99aa0a2366f5aa38a709e426e4f0be3978397a9e
Jelle Bakker <jellebakker@jb-audio.com>
2023-12-08 13:24:14 +0100

    'just gen' on macOS

--- native/src/bridge_generated.web.rs
+++ native/src/bridge_generated.web.rs
@@ -18,3 +18,12 @@ pub fn wire_rust_release_mode(port_: MessagePort) {
 // Section: impl Wire2Api
 
 // Section: impl Wire2Api for JsValue
+
+impl<T> Wire2Api<Option<T>> for JsValue
+where
+    JsValue: Wire2Api<T>,
+{
+    fn wire2api(self) -> Option<T> {
+        (!self.is_null() && !self.is_undefined()).then(|| self.wire2api())
+    }
+}
